adb22f4a80b840d8fd04eb9809c37f9acfa6c781
Greg Bailey <gregbailey617@gmail.com>
2024-10-06 10:37:19 -0400

    added backend for watched products

--- controllers/api/productRoutes.js
+++ controllers/api/productRoutes.js
@@ -1,12 +1,14 @@
 const router = require('express').Router();
-const { Product, User, Category } = require('../../models');
+const { Product, User, Category, WatchedProduct, PurchasedProduct } = require('../../models');
+
+
 
 
 
 // router.post('/', withAuth, async (req, res) => {
 
 router.post('/', async (req, res) => {
-  console.log(req.body)
+
   try {
       const newProduct = await Product.create({ ...req.body });
     
@@ -35,7 +37,104 @@ router.get('/:category_id', async (req,res) => {
   }
 })
 
+router.post('/:product_id/watched', async (req, res) => {
+  try {
+    const userId = req.body.user_id
+    const productId = req.params.product_id   
+
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.json('User not found');
+    }
+ 
+    // Find the product by ID
+    const product = await Product.findByPk(productId);
+    if (!product) {
+      return res.json('Product not found');
+    }
+
+    // Add the product to the user's watched products
+    const data = { user_id: userId, product_id: productId }
+    const watchedProduct = await WatchedProduct.create(data);
+
+    //Why does this not console.log this?
+    console.log(`Product (ID: ${productId}) has been added to User (ID: ${userId})'s watched products.`);
+
+    res.send(watchedProduct)
+
+  } catch {
+    console.error('Error adding product to watched products:', error);
+
+  }
+})
+
+router.post('/:product_id/purchased', async (req, res) => {
+  try {
+    const userId = req.body.user_id
+    const productId = req.params.product_id   
+
+    const user = await User.findByPk(userId);
+    if (!user) {
+      return res.json('User not found');
+    }
+ 
+    // Find the product by ID
+    const product = await Product.findByPk(productId);
+    if (!product) {
+      return res.json('Product not found');
+    }
+
+    // Add the product to the user's watched products
+    const data = { user_id: userId, product_id: productId }
+    const purchasedProduct = await PurchasedProduct.create(data);
+
+    //Why does this not console.log this?
+    console.log(`Product (ID: ${productId}) has been added to User (ID: ${userId})'s purchased products.`);
+
+    res.send(PurchasedProduct)
+
+  } catch {
+    console.error('Error adding product to watched products:', error);
+
+  }
+})
+
+
+
+
+
+
 
 
 module.exports = router;
 
+
+
+
+
+
+
+// }
+  
+// async function addProductToWatched(userId, productId) {
+// try {
+//   // Find the user by ID
+//   const user = await User.findByPk(userId);
+//   if (!user) {
+//     throw new Error('User not found');
+//   }
+
+//   // Find the product by ID
+//   const product = await Product.findByPk(productId);
+//   if (!product) {
+//     throw new Error('Product not found');
+//   }
+
+//   // Add the product to the user's watched products
+//   await user.addWatched_product(product);
+
+//   console.log(`Product (ID: ${productId}) has been added to User (ID: ${userId})'s watched products.`);
+// } catch (error) {
+//   console.error('Error adding product to watched products:', error);
+// }
+// )
--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -5,7 +5,7 @@ router.post('/', async (req, res) => {
   try {
     console.log(req.body);
     const userData = await User.create(req.body);
-    console.log(userData);
+
 
     req.session.save(() => {
       req.session.user_id = userData.id;
